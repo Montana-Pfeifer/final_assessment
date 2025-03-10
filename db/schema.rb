7e4775717d8288baa4e92bd8e7c0e0072f6e705d
Montana Pfeifer <montanapfeifer@hotmail.com>
2025-03-10 13:30:58 -0600

    migrations migrated updated schema

---
+++ db/schema.rb
@@ -0,0 +1,69 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[7.1].define(version: 2025_03_10_192655) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "customer_subscriptions", id: false, force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "subscription_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "subscription_id"], name: "index_customer_subscriptions_on_customer_id_and_subscription_id", unique: true
+    t.index ["customer_id"], name: "index_customer_subscriptions_on_customer_id"
+    t.index ["subscription_id"], name: "index_customer_subscriptions_on_subscription_id"
+  end
+
+  create_table "customer_teas", id: false, force: :cascade do |t|
+    t.bigint "customer_id", null: false
+    t.bigint "tea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id", "tea_id"], name: "index_customer_teas_on_customer_id_and_tea_id", unique: true
+    t.index ["customer_id"], name: "index_customer_teas_on_customer_id"
+    t.index ["tea_id"], name: "index_customer_teas_on_tea_id"
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", null: false
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_customers_on_email", unique: true
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.string "title", null: false
+    t.decimal "price", precision: 10, scale: 2, null: false
+    t.string "status"
+    t.string "frequency", comment: "Daily, Weekly, Monthly, etc."
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teas", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "description"
+    t.integer "temperature", comment: "Ideal brewing temperature in Fahrenheit or Celsius"
+    t.integer "brew_time", comment: "Recommended brewing time in minutes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "customer_subscriptions", "customers"
+  add_foreign_key "customer_subscriptions", "subscriptions"
+  add_foreign_key "customer_teas", "customers"
+  add_foreign_key "customer_teas", "teas"
+end
